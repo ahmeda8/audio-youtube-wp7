50e8c4d4d5ce2d204f9d4acd9d758b9038b343b3
ahmeda8 <ahmeda8@outlook.com>
2012-11-10 23:03:49 -0800

    changes to downloader

--- Online_YT_API/Utilities/Downloader.cs
+++ Online_YT_API/Utilities/Downloader.cs
@@ -2,34 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Phone.BackgroundTransfer;
 
 namespace Resources
 {
-    class Downloader : IDownloader
+    public class Downloader : IDownloader
     {
+        // constants
+        private BackgroundTransferRequest BTR;
+        private bool Aborted = true;
+        private Messaging Message;
+        private MP3FileUrlFetch UrlFetcher;
+        private Entry _Current;
 
+        //events
         public event GenericEvntHandler Completed;
 
-        public void Start()
+        //Construct 
+
+        public Downloader(Messaging MsgClass)
         {
-            throw new NotImplementedException();
+            Message = MsgClass;
+            UrlFetcher = new MP3FileUrlFetch();
+            UrlFetcher.Completed += UrlFetcher_Completed;
+        }
+
+        public void Start(Entry thisone)
+        {
+            Current = thisone;
+            UrlFetcher.StartFetch(Current.Source);
+            Message.SetMessage("Converting. "+Current.Title);
+        }
+
+        // event catcher
+
+        void UrlFetcher_Completed(object sender, APICompletedEventArgs e)
+        {
+            Uri Dest = new Uri("shared/transfers/temp"+sender.GetHashCode()+".mp3", UriKind.Relative);
+            Uri Src = new Uri(e.Response, UriKind.Absolute);
+            BTR = new BackgroundTransferRequest(Src, Dest);
+            BTR.Tag = Current.PlaylistID+"/"+Current.Id+".mp3";
+            BackgroundTransferService.Add(BTR);
+            BTR.TransferStatusChanged += BTR_TransferStatusChanged;
+            Message.SetMessage("Downloading. " + Current.Title);
+        }
+
+        void BTR_TransferStatusChanged(object sender, BackgroundTransferEventArgs e)
+        {
+            switch (e.Request.TransferStatus)
+            {
+                case TransferStatus.Completed :
+                    if (e.Request.StatusCode != 0)
+                    {
+                        ISOHelper.MoveFileOverwrite(e.Request.DownloadLocation.OriginalString, e.Request.Tag);
+                        BackgroundTransferService.Remove(e.Request);
+                        Message.SetMessage("Completed. "+Current.Title);
+                    }
+                    break;
+            }
         }
 
         public void Abort()
         {
-            throw new NotImplementedException();
+            if (BackgroundTransferService.Find(BTR.RequestId) != null)
+            {
+                BackgroundTransferService.Remove(BTR);
+                Message.SetMessage("Aborted. " + Current.Title);
+            }
         }
 
 
-        public Entry Current
+        private Entry Current
         {
             get
             {
-                throw new NotImplementedException();
+                return _Current;
             }
             set
             {
-                throw new NotImplementedException();
+                _Current = value;
             }
         }
     }
