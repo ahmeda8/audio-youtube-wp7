10e3fc3104512aa404a5fb23cd3c02ab6099ee82
ahmeda8 <ahmeda8@outlook.com>
2012-11-10 21:12:06 -0800

    adding messageing class

---
+++ Online_YT_API/Interfaces/IMessaging.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace Resources
+{
+    interface IMessaging
+    {
+        event ApiCompletedEventHandler Changed;
+        void SetMessage(string Message);
+        string GetMessage();
+    }
+}
---
+++ Online_YT_API/Utilities/Messaging.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Resources
+{
+    public class Messaging : IMessaging
+    {
+        private string message;
+
+        public void SetMessage(string Message)
+        {
+            this.message = Message;
+            RaiseChanged();
+        }
+
+        public string GetMessage()
+        {
+            return this.message;
+        }
+
+        public event ApiCompletedEventHandler Changed;
+
+        private virtual void RaiseChanged()
+        {
+            if (Changed != null)
+                Changed(this, new APICompletedEventArgs(message));
+
+        }
+    }
+}
