04a347f305dc3998ebc56805a8cffbb526276b04
ahmeda8 <ahmeda8@outlook.com>
2012-11-10 22:01:31 -0800

    adding new downloader

---
+++ Online_YT_API/Interfaces/IDownloader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace Resources
+{
+    interface IDownloader
+    {
+        event GenericEvntHandler Completed;
+        void Start();
+        void Abort();
+        Entry Current {get;set;}
+    }
+}
---
+++ Online_YT_API/Utilities/Downloader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Resources
+{
+    class Downloader : IDownloader
+    {
+
+        public event GenericEvntHandler Completed;
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Abort()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Entry Current
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
